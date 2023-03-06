2f4a2820389b63d542cb6eac745ae7abbe557e4d
Marco Bozzi <marcobo97@hotmail.it>
2023-03-06 16:02:09 +0100

    Heredoc

--- Src/executor2.c
+++ Src/executor2.c
@@ -6,7 +6,7 @@
 /*   By: mbozzi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 20:24:45 by mbozzi            #+#    #+#             */
-/*   Updated: 2023/03/04 19:33:10 by mbozzi           ###   ########.fr       */
+/*   Updated: 2023/03/06 15:54:19 by mbozzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,28 @@
 
 void	heredoc_fork(t_data **ms, char *cmd)
 {
-	int		fd;
 	int		status;
 	char	**cmds;
 
 	cmds = ft_calloc(sizeof(char *), 2);
 	cmds[0] = ft_strdup(cmd);
-	fd = open(HERED, O_RDONLY);
+	(*ms)->fd = open(HERED, O_RDONLY);
 	(*ms)->pid = fork();
 	if (!(*ms)->pid)
 	{
 		signal(SIGINT, SIG_DFL);
-		dup2(fd, STDIN_FILENO);
-		close(fd);
+		signal(SIGQUIT, SIG_DFL);
+		dup2((*ms)->fd, STDIN_FILENO);
+		close((*ms)->fd);
 		execve(cmd, cmds, NULL);
 		perror("smashell");
 		exit (EXIT_FAILURE);
 	}
 	else if ((*ms)->pid > 0)
 	{
-		close(fd);
+		close((*ms)->fd);
 		waitpid((*ms)->pid, &status, 0);
 	}
-	if (!access(HERED, F_OK))
-		unlink(HERED);
 	free(cmds[0]);
 	free(cmds);
 	return ;
