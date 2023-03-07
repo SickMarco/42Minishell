611465b9f273b24ddcf9e9a3432204415e8b4982
mrbff <marco.baffo@live.it>
2023-03-07 17:26:07 +0100

    pipe sdnw

--- Src/pipe2.c
+++ Src/pipe2.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabaffo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/03 14:19:29 by mabaffo           #+#    #+#             */
-/*   Updated: 2023/03/05 19:16:44 by mabaffo          ###   ########.fr       */
+/*   Created: 2023/03/07 16:22:58 by mabaffo           #+#    #+#             */
+/*   Updated: 2023/03/07 17:20:01 by mabaffo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ char **env;
 typedef struct s_cmd
 {
 	char	**arg;
+	char	*cmd_path;
 	int		pipe_fds[2];
 	int		prev_pipe_fds[2];
 	int		in_fd;
@@ -95,18 +96,47 @@ void	ft_fillarg(t_cmd *cmd, t_list **lst)
 void	ft_freearg(t_cmd *cmd)
 {
 	char	**arg;
-	int		i;
+//	int		i;
 
 	arg = cmd->arg;
 	if (!arg)
 		return ;
-	i = 0;
+/*	i = 0;
 	while (arg[i])
 	{
 		free(arg[i]);
 		i++;
-	}
+	}*/
 	free(arg);
+}
+
+char	**path_init(void)
+{
+	char	**path;
+	int		i;
+
+	i = -1;
+	path = ft_split(getenv("PATH"), ':');
+	while (path[++i])
+		path[i] = ft_strjoin2(path[i], "/");
+
+	return (path);
+}
+
+char	*path_search(char **path, char *cmd)
+{
+	int		i;
+	char	*cmd_path;
+
+	i = -1;
+	while (path[++i] && ft_strncmp(cmd, ".", 1))
+	{
+		cmd_path = ft_strjoin(path[i], cmd);
+		if (!access(cmd_path, F_OK))
+			return (cmd_path);
+		free(cmd_path);
+	}
+	return (NULL);
 }
 
 void execute_multi_pipe(t_list *lst)
@@ -116,7 +146,9 @@ void execute_multi_pipe(t_list *lst)
 	t_list	*head;
 	int		pid;
 	t_cmd	cmd;
+	char	**path;
 
+	path = path_init();
 	head = lst;
 	cmd.in_fd = STDIN_FILENO;
 	cmd.out_fd = STDOUT_FILENO;
@@ -134,12 +166,17 @@ void execute_multi_pipe(t_list *lst)
 			 *	and forks a child process using fork() */
 			if (pid == 0)//child
 			{
+				close(cmd.pipe_fds[0]);//gpt
+				dup2(cmd.pipe_fds[1], STDOUT_FILENO);
+				close(cmd.pipe_fds[1]);
+
 				/* In the child process, the read end of the previous pipe is closed
 				 * and its file descriptor is duplicated onto
 				 * the standard input file descriptor (STDIN_FILENO) using dup2().*/
 				close(cmd.prev_pipe_fds[1]);
 				dup2(cmd.prev_pipe_fds[0], STDIN_FILENO);
 				close(cmd.prev_pipe_fds[0]);
+
 				/* If the current output file descriptor is not equal to standard output (STDOUT_FILENO),
 				* its file descriptor is duplicated onto the standard output file descriptor using dup2().*/
 				if (cmd.out_fd != STDOUT_FILENO)
@@ -147,7 +184,9 @@ void execute_multi_pipe(t_list *lst)
 					dup2(cmd.out_fd, STDOUT_FILENO);
 					close(cmd.out_fd);
 				}
-				execve((cmd.arg)[0], cmd.arg, env);//Then execve() is called to execute the current command.
+				cmd.cmd_path = path_search(path, cmd.arg[0]);
+				execve(cmd.cmd_path, cmd.arg, env);//Then execve() is called to execute the current command.
+				free(cmd.cmd_path);
 				exit(EXIT_FAILURE);
 			}
 
@@ -159,8 +198,9 @@ void execute_multi_pipe(t_list *lst)
 			 * and its file descriptor is set to the write end of the current pipe. */
 				close(cmd.prev_pipe_fds[0]);
 				close(cmd.out_fd);
+				waitpid(pid, NULL, 0); 
 				cmd.out_fd = cmd.prev_pipe_fds[1];
-
+				close(cmd.prev_pipe_fds[1]);///
 				ft_freearg(&cmd);///
 //				arg_count = 0;//The argument count is set to 0 to prepare for the next command.
 			}
@@ -186,7 +226,7 @@ void execute_multi_pipe(t_list *lst)
 			lst = lst->next;
 			if (!lst)
 				break ;
-			cmd.out_fd = open((char *)(lst->next->content), O_WRONLY | O_CREAT | O_TRUNC, 0666);
+			cmd.out_fd = open((char *)(lst->content), O_WRONLY | O_CREAT | O_TRUNC, 0666);
 			if (cmd.out_fd < 0)
 				ft_perrex("open");
 			lst = lst->next;
@@ -197,33 +237,35 @@ void execute_multi_pipe(t_list *lst)
 			lst = lst->next;
 			if (!lst)
 				break ;
-			cmd.out_fd = open((char *)(lst->next->content), O_WRONLY | O_CREAT | O_APPEND, 0666);
+			cmd.out_fd = open((char *)(lst->content), O_WRONLY | O_CREAT | O_APPEND, 0666);
 			if (cmd.out_fd < 0)
 				ft_perrex("open");
 			lst = lst->next;
 		}
 		else
 		{
-			printf("F fillarg\n");
+	//		printf("F fillarg\n");
 			ft_fillarg(&cmd, &lst);
-			printf("%s\n", cmd.arg[0]);
+	//		printf("%s\n", cmd.arg[0]);
 		}
 	}
 //	args[arg_count] = NULL;
 	pid = fork();///int
 	if (pid == 0)//child
 	{
-		printf("F !\n");
+	//	printf("F !\n");
 		dup2(cmd.in_fd, STDIN_FILENO);
 		if (cmd.out_fd != STDOUT_FILENO)
 			dup2(cmd.out_fd, STDOUT_FILENO);
-		printf("%s\n", cmd.arg[0]);
-		execve("/usr/bin/ls"/*cmd.arg[0]*/, cmd.arg, env);
+//		printf("%s\n", cmd.arg[0]);
+		cmd.cmd_path = path_search(path, cmd.arg[0]);
+		execve(cmd.cmd_path, cmd.arg, env);
+		free(cmd.cmd_path);
 		exit(EXIT_FAILURE);
 	}
 	else if (pid > 0)//padre
 	{
-		printf("F ?"); 
+	//	printf("F ?"); 
 		waitpid(pid, NULL, 0);
 		close(cmd.in_fd);
 		close(cmd.out_fd);
@@ -239,9 +281,10 @@ int main(int ac, char **av, char **envp)
 	(void)(ac);
 	(void)(av);
 	env = envp;
-	char *s = ft_strdup("ls -a");// | grep .c > output.txt | cat < output.txt");
+	char *s = ft_strdup("ls -a | wc -l");// | grep .c > output.txt | cat < output.txt");
 	t_list *lst = ft_subsplit((ft_split1(s)));
-	ft_print_slst(lst);
+//	ft_print_slst(lst);
 	execute_multi_pipe(lst);
 	return 0;
 }
+
