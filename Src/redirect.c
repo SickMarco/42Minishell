4f5425e0b44ef82f97accec45cf4464ee3fc0027
Marco Bozzi <marcobo97@hotmail.it>
2023-03-16 15:51:53 +0100

    Redir Fix

--- Src/redirect.c
+++ Src/redirect.c
@@ -6,7 +6,7 @@
 /*   By: mbozzi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 17:34:27 by mbozzi            #+#    #+#             */
-/*   Updated: 2023/03/13 18:47:18 by mbozzi           ###   ########.fr       */
+/*   Updated: 2023/03/16 15:50:04 by mbozzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,8 @@ void	close_redir(t_data **ms, t_cmd *cmd)
 	if (ft_strncmp(cmd->cmds[0], "cd", 3))
 	{
 		if (cmd->out_fd != -1)
-		{
 			dup2((*ms)->stdout_fd, STDOUT_FILENO);
-			close((*ms)->stdout_fd);
-		}
 		if (cmd->in_fd != -1)
-		{
 			dup2((*ms)->stdin_fd, STDIN_FILENO);
-			close((*ms)->stdin_fd);
-		}
 	}
 }
